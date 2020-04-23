a50724a4329e257d98f05d6064d6436fba2e7068
jcklpe <david@jackalope.tech>
2020-04-23 16:01:01 -0500

    updated build process to gulp 4

---
+++ assets/src/js/index.js
@@ -0,0 +1,17 @@
+//- IMPORT DATA
+//import("./atx-data-pull.js");
+
+//- VENDORS
+// modern-admin vendors
+//import("../vendor/modern-admin/js/data/jvector/visitor-data.js");
+//import("../vendor/modern-admin/js/vendors/vendors.min.js");
+//import("../vendor/modern-admin/js/vendors/charts/chart.min.js");
+//import("../vendor/modern-admin/js/vendors/charts/raphael-min.js");
+//import("../vendor/modern-admin/js/vendors/charts/morris.min.js");
+//import("../vendor/modern-admin/js/vendors/charts/jvector/jquery-jvectormap-2.0.3.min.js");
+//import("../vendor/modern-admin/js/vendors/charts/jvector/jquery-jvectormap-world-mill.js");
+
+// modern-admin core
+//import("../vendor/modern-admin/js/core/app-menu.js");
+//import("../vendor/modern-admin/js/core/app.js");
+//import("../vendor/modern-admin/js/scripts/pages/dashboard-sales.js");
--- gulpfile.js
+++ gulpfile.js
@@ -1,143 +1,157 @@
 // Gulp.js configuration
 "use strict";
 
-//source and build folders
+// options for src and build folders
 const dir = {
-  src: "./assets/src/",
-  build: "./assets/build/",
-  root: "./"
-},
-  // Gulp and plugins
+    src: "./assets/src/",
+    build: "./assets/build/",
+    vendor: "./assets/vendor/",
+    root: "./",
+    // Replace with URL of your local site
+    localDevURL: "dev-colab.test/"
+  },
+  // gulp plugins etc
   gulp = require("gulp"),
   gutil = require("gulp-util"),
-  imagemin = require("gulp-imagemin"),
   sass = require("gulp-sass"),
-  postcss = require("gulp-postcss"),
-  // concat = require("gulp-concat"),
-  stripdebug = require("gulp-strip-debug"),
-  uglify = require("gulp-uglify"),
-  sourcemaps = require("gulp-sourcemaps"),
-  // babel = require("gulp-babel"),
+  cssnano = require("cssnano"),
+  autoprefixer = require("gulp-autoprefixer"),
   gulpImport = require("gulp-imports"),
-  livereload = require('gulp-livereload');
+  sourcemaps = require("gulp-sourcemaps"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat"),
+  rename = require("gulp-rename"),
+  plumber = require("gulp-plumber"),
+  babel = require("gulp-babel"),
+  postcss = require("gulp-postcss"),
+  browserSync = require("browser-sync").create();
 
-
-
+// Browser-sync
+var browsersync = false;
 
 //- CSS
 //config
-var css = {
+var scss = {
   src: dir.src + "scss/*.scss",
-  watch: dir.src + "scss/**/*.scss",
-  build: dir.build,
+  watch: dir.src + "scss/**/*.scss", //*/
+  build: dir.build + "scss/",
   sassOpts: {
     outputStyle: "expanded",
     //   imagePath       : images.build,
     precision: 3,
-    errLogToConsole: true,
-    includePaths: ['scss/**/*.scss']
-
+    errLogToConsole: true
   },
   processors: [
     require("postcss-assets")({
       // loadPaths: ['images/'],
-      basePath: dir.build,
-      baseUrl: "/"
+      basePath: dir.build
     }),
-    require("autoprefixer")({
-      browsers: ["last 2 versions", "> 2%"]
-    }),
+    require("autoprefixer")(),
     require("css-mqpacker"),
     require("cssnano")
   ]
 };
 
-// CSS processing task
-gulp.task("css", () =>
-{
-  return gulp
-    .src(css.src)
-    .pipe(sourcemaps.init())
-    .pipe(sass(css.sassOpts))
-    .pipe(postcss(css.processors))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(css.build))
-    .pipe(livereload());
-});
+// SCSS processing
+gulp.task(
+  "scss",
+  gulp.series(() => {
+    return gulp
+      .src(scss.src)
+      .pipe(sourcemaps.init())
+      .pipe(sass(scss.sassOpts))
+      .pipe(postcss(scss.processors))
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest(scss.build))
+      .pipe(
+        browsersync
+          ? browsersync.reload({
+              stream: true
+            })
+          : gutil.noop()
+      );
+  })
+);
 
-//-Javascript
-//head config
-const jshead = {
-  src: dir.src + "js/head.js", //*/
-  build: dir.build + "./",
-  watch: dir.src + "js/**/*.js" //*/
+//- Javascript
+//config
+var js = {
+src: dir.src + "js/*.js",
+watch: dir.src + "js/**/*.js",
+build: dir.build + "js/",
 };
 
-// JS head processing task
-gulp.task("jshead", function ()
-{
-  return gulp
-    .src(jshead.src)
-    .pipe(sourcemaps.init())
-    .pipe(gulpImport())
-    //.pipe(uglify())
-    //.pipe(stripdebug())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(jshead.build))
-    .pipe(livereload());
-});
-
-// footer config
-const jsfooter = {
-  src: dir.src + "js/footer.js", //*/
-  build: dir.build + "./",
-  watch: dir.src + "js/**/*.js" //*/
-};
-
-// JS footer processing task
-gulp.task("jsfooter", function ()
-{
-  return gulp
-    .src(jsfooter.src)
-    .pipe(sourcemaps.init())
-    .pipe(gulpImport())
-    .pipe(uglify())
-    .pipe(stripdebug())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(jsfooter.build));
-});
-
-gulp.task("js", ["jshead", "jsfooter"]);
-
-//- Build
-gulp.task("build", ["css", "js"]);
-
-//- Watch
-
-gulp.task("watch", () =>
-{
-  livereload.listen();
-  gulp.watch(css.watch, ["css"]);
-
-  gulp.watch(jshead.watch, ["jshead"]);
-
-  gulp.watch(jsfooter.watch, ["jsfooter"]);
+// concat and minify JavaScript
+gulp.task(
+  "js",
+  gulp.series(() => {
+    return gulp
+      .src(js.src)
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(gulpImport())
+      // .pipe(concat("scripts.js"))
+      .pipe(gulp.dest(js.build))
+      .pipe(
+        rename({
+          suffix: ".min"
+        })
+      )
+      // .pipe(uglify())
+      .pipe(sourcemaps.write(".")) // Creates sourcemap for minified JS
+      .pipe(gulp.dest(js.build))
+      .pipe(
+        browsersync
+          ? browsersync.reload({
+              stream: true
+            })
+          : gutil.noop()
+      );
+  })
+);
 
 
-});
 
-// default task
-gulp.task("default", ["build", "watch"]);
+// Browser-Sync watch files and inject changes
+gulp.task(
+  "dev",
+  gulp.series(() => {
+    // Watch files
+    var files = [
+      "./assets/build/scss/*.css",
+      "./assets/build/js/*.js",
+    ];
 
-//- Image Uploads optimization
+    browserSync.init(files, {
 
-gulp.task("image", () =>
-  gulp
-    .src("../../uploads/**/*")
-    .pipe(
-      imagemin({
-        verbose: true
-      })
-    )
-    .pipe(gulp.dest("../../uploads/"))
+      proxy: dir.localDevURL
+    });
+
+    //watch scss
+      gulp.watch(scss.watch, gulp.series("scss"));
+
+    // watch js
+    gulp
+      .watch("./assets/src/js/*.js", gulp.series("js"))
+      .on("change", browserSync.reload);
+  })
+);
+
+// Watch files for changes (without Browser-Sync)
+gulp.task(
+    "watch",
+    gulp.series(() => {
+        // Watch .scss files
+        gulp.watch(["./assets/src/scss/**/*.scss"], gulp.series("scss"));
+
+        // Watch site-js files
+        gulp.watch("./assets/src/js/*.js", gulp.series("js"));
+    })
+);
+
+// Run scss and js compilation
+gulp.task(
+  "default",
+    gulp.series("scss", "js")
+
 );
